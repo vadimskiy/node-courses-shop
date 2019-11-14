bc06b15f97691e53ff6373b3784ff4dc879e29fd
vadim_sapovsky <v.sapovskiy@gmail.com>
2019-11-14 23:26:55 +0300

    setup handlebars

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 
 const PORT = process.env.PORT || 3000;
 
