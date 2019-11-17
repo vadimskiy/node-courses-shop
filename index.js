70c74212c3b08899389a54383b374679c3f02624
vadim_sapovsky <v.sapovskiy@gmail.com>
2019-11-17 15:16:08 +0300

    Add routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
 
 const app = express();
 const hbs = exphbs.create({
@@ -13,11 +16,9 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.static('public'));
-
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
 
 
 const PORT = process.env.PORT || 3000;
