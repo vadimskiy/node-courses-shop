a20d999250740f419440a3166d6c0658d687c2b1
vadim_sapovsky <v.sapovskiy@gmail.com>
2019-11-19 20:55:22 +0300

    Create course model

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const router = Router();
+const Course = require('../models/course');
 
 router.get('/', (req, res) => {
     res.render('add', {
@@ -8,8 +9,11 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-    console.log(req.body);
+router.post('/', async (req, res) => {
+    const {title, price, img} = req.body;
+    const course = new Course(title, price, img);
+
+    await course.save();
 
     res.redirect('courses');
 });
