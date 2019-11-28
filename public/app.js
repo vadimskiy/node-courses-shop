4f8ba7b7a9de50ffedad9cd46588ed5a0028232d
vadim_sapovsky <v.sapovskiy@gmail.com>
2019-11-28 20:57:36 +0300

    Add remove method

--- public/app.js
+++ public/app.js
@@ -4,3 +4,19 @@ document.querySelectorAll('.price').forEach(node => {
         style: 'currency'
     }).format(node.textContent)
 });
+
+const cartComponent = document.querySelector('#cart');
+if (cartComponent) {
+    cartComponent.addEventListener('click', event => {
+        if (event.target.classList.contains('js-remove')) {
+            const id = event.target.dataset.id;
+
+            fetch('/cart/remove/' + id, {
+                method: 'delete'
+            }).then(res => res.json())
+              .then(cart => {
+                  console.log(cart);
+              });
+        }
+    });
+}
